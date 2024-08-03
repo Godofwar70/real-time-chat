50515df98d7e3ffd81ddd22634dad06f11b4709b
Godofwar70 <brunob_21@hotmail.es>
2024-08-02 20:08:37 -0500

    cambios real time

--- server/index.js
+++ server/index.js
@@ -1,3 +1,63 @@
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const userRoutes = require('./routes/userRoutes');
+// const messageRoutes = require('./routes/messageRoutes');
+// const socketIo = require('socket.io');
+
+// const app = express();
+// require('dotenv').config();
+
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Permitir solicitudes desde este origen
+//   credentials: true, // Permitir credenciales (cookies, encabezados de autorización, etc.)
+// }));
+// app.use(express.json());
+
+// app.use('/api/auth', userRoutes);
+// app.use('/api/messages', messageRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1/chats';
+
+// mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log("DB CONNECTION SUCCESSFUL");
+//     if (process.env.NODE_ENV !== 'test') {
+//       app.listen(PORT, () => {
+//         console.log(`Server started on ${PORT}`);
+//       });
+//     }
+//   })
+//   .catch(err => {
+//     console.error('Connection error', err);
+//   });
+
+// const io = socketIo(server, {
+//   cors: {
+//     origin: 'http://localhost:3000', // Permitir solicitudes desde este origen
+//     credentials: true, // Permitir credenciales (cookies, encabezados de autorización, etc.)
+//   },
+// });
+
+// global.onlineUsers = new Map();
+
+// io.on("connection", (socket) => {
+//   global.chatSocket = socket;
+//   socket.on('add-user', (userId) => {
+//     onlineUsers.set(userId, socket.id);
+//   });
+
+//   socket.on('send-msg', (data) => {
+//     const sendUserSocket = onlineUsers.get(data.to);
+//     if (sendUserSocket) {
+//       socket.to(sendUserSocket).emit('msg-recieve', data.message); // Asegúrate de que se use correctamente data.message aquí
+//     }
+//   });
+// });
+
+// module.exports = app;
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -17,40 +77,43 @@ app.use(express.json());
 app.use('/api/auth', userRoutes);
 app.use('/api/messages', messageRoutes);
 
-const uri = "mongodb://localhost:27017/chats";
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1/chats';
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("DB CONNECTION SUCCESSFUL");
-}).catch((err) => {
-  console.log(err.message);
-});
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB CONNECTION SUCCESSFUL");
+    if (process.env.NODE_ENV !== 'test') {
+      const server = app.listen(PORT, () => {
+        console.log(`Server started on ${PORT}`);
+      });
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server started on ${process.env.PORT}`);
-});
+      const io = socketIo(server, {
+        cors: {
+          origin: 'http://localhost:3000', // Permitir solicitudes desde este origen
+          credentials: true, // Permitir credenciales (cookies, encabezados de autorización, etc.)
+        },
+      });
 
-const io = socketIo(server, {
-  cors: {
-    origin: 'http://localhost:3000', // Permitir solicitudes desde este origen
-    credentials: true, // Permitir credenciales (cookies, encabezados de autorización, etc.)
-  },
-});
+      global.onlineUsers = new Map();
 
-global.onlineUsers = new Map();
+      io.on("connection", (socket) => {
+        global.chatSocket = socket;
+        socket.on('add-user', (userId) => {
+          onlineUsers.set(userId, socket.id);
+        });
 
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on('add-user', (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
-
-  socket.on('send-msg', (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit('msg-recieve', data.message); // Asegúrate de que se use correctamente data.message aquí
+        socket.on('send-msg', (data) => {
+          const sendUserSocket = onlineUsers.get(data.to);
+          if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-recieve', data.message); // Asegúrate de que se use correctamente data.message aquí
+          }
+        });
+      });
     }
+  })
+  .catch(err => {
+    console.error('Connection error', err);
   });
-});
+
+module.exports = app; // Exporta la aplicación Express
---
+++ server/tests/setup.js
@@ -0,0 +1,38 @@
+// tests/setup.js
+
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongoServer;
+
+const connect = async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+
+  await mongoose.disconnect(); // Desconectar antes de conectar a la nueva base de datos de prueba
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+const closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+};
+
+const clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+};
+
+module.exports = {
+  connect,
+  closeDatabase,
+  clearDatabase,
+};
